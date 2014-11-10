b35aeb34bf31dd627cfdb5447fcc441e01c803f7
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-11-10 20:44:53 +0100

    test added

---
+++ tests/kit.js
@@ -0,0 +1,23 @@
+var _ = require('./../src/kit.js');
+
+// using assert passed to the test function that just logs failures
+exports['test that logs kit framework'] = function(assert) {
+    var merge = _.merge(
+        { a: 'aa' },
+        { a: 'aaa' },
+        { a: 'aa' },
+        { a: 'aa' }
+    );
+    
+    assert.equal(merge.a, 'aa', 'Assert merge should be true');
+    
+    assert.equal(_.isObject({}), true, 'Assert isObject should be true');
+    assert.equal(_.isObject(function() {}), false, 'Assert isObject should be false');
+    
+    assert.equal(_.isFunction(function() {}), true, 'Assert isFunction should be true');
+    assert.equal(_.isFunction(_), false, 'Assert isFunction should be false');
+;}
+
+if (module == require.main) {
+    require('test').run(exports);
+}

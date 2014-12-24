4bed02e7ea5966f711e3aeb4416e93c3e3212483
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-12-24 11:46:46 +0100

    concat added

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
 
 gulp.task('kit', function () {
     return gulp.src('src/kit.js')
+        .pipe(concat('kit.min.js'))
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(uglify())

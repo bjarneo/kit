17121ed5288293f66ec0d7c3f6bb4d97968e958c
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-12-24 11:40:26 +0100

    saved object prototype and toString to variables

--- src/kit.js
+++ src/kit.js
@@ -4,13 +4,16 @@
 
 (function () {
     'use strict';
-    
+
     var _ = {};
+
+    var ObjProto = Object.prototype,
+        toString = ObjProto.toString;
 
     // Recursively merge multiple objects to one
     _.merge = function (object) {
         var i = 1, key;
-        
+
         if (!arguments.length) {
             throw 'Error: No objects added to merge method!';
         }
@@ -22,7 +25,7 @@
                         if (!this.isObject(object[key])) {
                             object[key] = {};
                         }
-                        
+
                         object[key] = this.merge(object[key], arguments[i][key]);
                     } else {
                         object[key] = arguments[i][key];
@@ -33,98 +36,98 @@
 
         return object;
     };
-    
+
     _.isObject = function (object) {
-        if (Object.prototype.toString.call(object) === '[object Object]') {
+        if (toString.call(object) === '[object Object]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     _.isFunction = function (func) {
-        if (Object.prototype.toString.call(func) === '[object Function]') {
+        if (toString.call(func) === '[object Function]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     _.isUndefined = function (value) {
-        if (Object.prototype.toString.call(value) === '[object Undefined]') {
+        if (toString.call(value) === '[object Undefined]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     _.isNull = function (value) {
-        if (Object.prototype.toString.call(value) === '[object Null]') {
+        if (toString.call(value) === '[object Null]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     _.isArray = function (array) {
-        if (Object.prototype.toString.call(array) === '[object Array]') {
+        if (toString.call(array) === '[object Array]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     _.isDate = function (date) {
-        if (Object.prototype.toString.call(date) === '[object Date]') {
+        if (toString.call(date) === '[object Date]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     _.isRegExp = function (regexp) {
-        if (Object.prototype.toString.call(regexp) === '[object RegExp]') {
+        if (toString.call(regexp) === '[object RegExp]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     _.isString = function (string) {
-        if (Object.prototype.toString.call(string) === '[object String]') {
+        if (toString.call(string) === '[object String]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     _.isNumber = function (number) {
-        if (Object.prototype.toString.call(number) === '[object Number]') {
+        if (toString.call(number) === '[object Number]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     _.isInteger = function (integer) {
         return parseInt(integer) === integer;
     };
-    
+
     _.isFloat = function (number) {
-            return number === +number && number !== (number|0);
+        return number === +number && number !== (number|0);
     };
-    
+
     _.isGlobal = function (input) {
-        if (Object.prototype.toString.call(input) === '[object global]') {
+        if (toString.call(input) === '[object global]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     // Check if is node, amd or else we set it to window
     if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = _; 
+        module.exports = _;
     } else if (typeof define === 'function' && define.amd) {
         define([], function() {
                 return _;
@@ -132,4 +135,4 @@
     } else {
         window._ = _;
     }
-})();
+}());

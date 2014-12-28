13b27c3915b539ee43bf19d1773295ca235b9735
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-12-28 22:00:07 +0100

    removed old tests

--- tests/test.js
+++ tests/test.js
@@ -41,6 +41,8 @@ describe('Kit library', function () {
                 }
             });
         });
+
+
     });
 
     describe('#first()', function () {
@@ -310,28 +312,3 @@ describe('Kit library', function () {
         });
     });
 });
-
-
-/*
-assert.equal(_.isObject({}), true, 'Assert isObject({}) should return true');
-assert.equal(_.isFunction(function() {}), true, 'Assert isFunction(function() {}) should return true');
-assert.equal(_.isUndefined(test), true, 'Assert isUndefined(test) should return true');
-assert.equal(_.isArray(['a', 'b', 'c', 'd']), true, 'Assert isArray(["a", "b", "c", "d"]) should return true');
-assert.equal(_.isDate(new Date()), true, 'Assert isDate(new Date) should return true');
-assert.equal(_.isRegExp(/a-z/), true, 'Assert isRegExp(/a-z/) should return true');
-assert.equal(_.isString('its a string! 1+1'), true, 'Assert isString("its a string! 1+1") should return true');
-assert.equal(_.isNumber(1), true, 'Assert isNumber(1) should return true');
-assert.equal(_.isNumber(1.2), true, 'Assert isNumber(1.2) should return true');
-assert.equal(_.isNumber(-1.2), true, 'Assert isNumber(-1.2) should return true');
-assert.equal(_.isInteger(1), true, 'Assert isInteger(1) should return true');
-assert.equal(_.isInteger(1.0), true, 'Assert isInteger(1.0) should return true');
-assert.equal(_.isInteger(1.1), false, 'Assert isInteger(1.1) should return false');
-assert.equal(_.isInteger('1'), false, 'Assert isInteger("1") should return false');
-assert.equal(_.isInteger([]), false, 'Assert isInteger([]) should return false');
-assert.equal(_.isGlobal(global), true, 'Assert isGlobal(global) should return true');
-};
-
-if (module == require.main) {
-require('test').run(exports);
-}
-*/
